4ffef297b72e48de26d5bc2258339aec76b591bf
Gabriel Dornas <gabrielbdornas@gmail.com>
2026-03-26 17:56:36 -0300

    feat(extract): download and save files in chunck
    extract api mode
    
    see #28

--- dpetl/extract/api.py
+++ dpetl/extract/api.py
@@ -1,5 +1,6 @@
 import requests
 from pathlib import Path
+from tqdm import tqdm
 
 def extract_api(resource, **kwargs):
     try:
@@ -20,9 +21,14 @@ def extract_api(resource, **kwargs):
             response.raise_for_status()
 
         Path(resource.path).parent.mkdir(parents=True, exist_ok=True)
+        total_length = int(response.headers.get('content-length', 0))
+        progress = tqdm(total=total_length, unit='iB', unit_scale=True)
 
         with open(resource.path, 'wb') as f:
-            f.write(response.content)
+            for data in response.iter_content(1024):  # 1KB
+                progress.update(len(data))
+                f.write(data)
+        progress.close()
 
     except requests.exceptions.RequestException as e:
         print(f'Error downloading file: {e}')
