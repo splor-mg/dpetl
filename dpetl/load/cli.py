845e0ef013bf26b9a43848f7be39e2d6335815dd
Maria Duque <maria.oliveira.duque@gmail.com>
2026-05-21 11:12:49 -0300

    refactor(load): remove CLI arguments

--- dpetl/load/cli.py
+++ dpetl/load/cli.py
@@ -7,20 +7,6 @@ def create_load_subcommands(subparsers):
         'load', help='Simplified some ETL load operations.'
     )
 
-    parser.add_argument(
-        '--private',
-        '-p',
-        action='store_true',
-        help='Create repositories as private instead of public.',
-    )
-
-    parser.add_argument(
-        '--org',
-        '-o',
-        action='store_true',
-        help='Indicates that the repositories should be created under an organization.',
-    )
-
     parser.set_defaults(func=handle_command)
 
     return parser
