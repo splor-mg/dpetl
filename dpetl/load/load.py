eb1ecefb46665b4101f6b5e33bde386993e63e31
Maria Duque <maria.oliveira.duque@gmail.com>
2026-05-21 11:18:11 -0300

    refactor(load): use descriptor parameters for repository configuration

--- dpetl/load/load.py
+++ dpetl/load/load.py
@@ -4,12 +4,12 @@ from pathlib import Path
 from frictionless import Package
 from dotenv import load_dotenv, find_dotenv
 
-from .github import repo_exists, create_repo, commit_files
+from .github import repo_exists, create_repo, commit_remote, commit_local
 
 logger = logging.getLogger(__name__)
 
 
-def load_package(descriptor, **kwargs):
+def load_package(package):
     """
     Load data and metadata from a datapackage into a GitHub repository.
     """
@@ -17,51 +17,52 @@ def load_package(descriptor, **kwargs):
     # Get environment variables
     load_dotenv(find_dotenv(usecwd=True))
     token = os.environ.get('GH_TOKEN')
-    owner = os.environ.get('GH_OWNER')
 
-    if not all([token, owner]):
-        logger.error(('Missing one of the required environment variables:'
-                      'GH_TOKEN or GH_OWNER.'))
+    if not token:
+        logger.error(('Missing required environment variables: GH_TOKEN.'))
         raise SystemExit(1)
 
-    # Prepare config, dataset version and repository paths
-    org = kwargs.get('org', False)
-    private = kwargs.get('private', False)
+    # Prepare config and repository paths
+    dpetl = package.custom.get('dpetl_load', {})
+    owner = dpetl.get('owner')
+    repo   = dpetl.get('repo')
+    level  = dpetl.get('level') or 'user'
+    visibility = dpetl.get('visibility') or 'private'
 
-    package = Package(descriptor)
-    datapackage = Path(descriptor)
+    if not owner:
+        logger.error('Missing required field "owner" in "dpetl_load".')
+        raise SystemExit(1)
 
-    year = get_max_year(package)
-    repo = f'{package.name}_{year}'
+    if level not in ('user', 'orgs'):
+        logger.error('Field "level" in "dpetl_load" must be "user" or "orgs".')
+        raise SystemExit(1)
+
+    if visibility not in ('public', 'private'):
+        logger.error('Field "visibility" in "dpetl_load" must be "public" or "private".')
+        raise SystemExit(1)
 
     logger.info(f'Processing {repo}.')
 
     # Ensure remote repository exists
     if not repo_exists(owner, repo, token):
         logger.info(f'Creating repository {repo}.')
-        create_repo(owner, repo, token, private=private, org=org)
+        create_repo(owner, repo, token, level, visibility)
 
     # Prepare files to send
     files = {}
     for resource in package.resources:
-        file = datapackage.parent / resource.path
+        [resource.custom.pop(key, None) for key in ['dpetl_extract', 'dpetl_transform']]
+        file = Path(package._basepath) / resource.path
         with open(file, 'rb') as f:
             files[resource.path] = f.read()
 
+    [package.custom.pop(key, None) for key in ['dpetl_transform', 'dpetl_load']]
     files['datapackage.json'] = package.to_json().encode()
 
     # Commit all files in a single commit
     logger.info('Committing data package.')
 
-    commit_files(owner, repo, token, files)
-
-
-def get_max_year(package):
-    """
-    Extract the maximum 'ano' value across all data resources.
-    """
-
-    return max(row['ano']
-               for resource in package.resources
-               for row in resource.read_rows()
-               if row.get('ano') is not None)
+    if repo:
+        commit_remote(owner, repo, token, files)
+    else:
+        commit_local(files)
